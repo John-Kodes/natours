a0a44cdfa105526f6171691f53f7a901789bcb91
John-Kodes <jdanielsemine@gmail.com>
2021-10-15 18:47:20 +0400

    Implemented CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -26,6 +27,12 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 ////////// 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+
+// Here we right a handler for the HTTP request 'options'. The route we want it to handle is all the routes
+app.options('*', cors());
+
 //// SERVING STATIC FILES
 // express.static() because we want to serve static files. url.com/overview.html now works. the 'public' from the URL is not needed because when we open a URL and it can't find in any of routes, it will then look into that public folder that we define and it kinda sets that folder to the route.
 app.use(express.static(path.join(__dirname, 'public')));
